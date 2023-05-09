85a36441a3e8b9ef47d8819902864c7009c24ea2
GabrielCP <gabrielpiresc2010@gmail.com>
2023-05-08 21:56:30 -0300

    Create topo_sw2_host4.py

---
+++ topo_sw2_host4.py
@@ -0,0 +1,32 @@
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import CPULimitedHost
+from mininet.link import TCLink
+
+class MyTopo(Topo):
+    def init(self):
+        Topo.init(self)
+
+        # Add hosts
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+        h3 = self.addHost('h3')
+        h4 = self.addHost('h4')
+
+        # Add switches
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+
+        # Add links
+        self.addLink(h1, s1, bw=10, delay='1ms', loss=0)
+        self.addLink(h3, s1, bw=10, delay='1ms', loss=0)
+        self.addLink(h2, s2, bw=10, delay='1ms', loss=0)
+        self.addLink(h4, s2, bw=10, delay='1ms', loss=0)
+        self.addLink(s1, s2, bw=10, delay='1ms', loss=0)
+
+if name == 'main':
+    topo = MyTopo()
+    net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink)
+    net.start()
+    net.pingAll()
+    net.stop()
